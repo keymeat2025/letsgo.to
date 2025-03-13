301e579b7c05485aea1e8173f2f05055a6dd8ad2
keymeat2025 <keymeat2025@gmail.com>
2025-03-13 12:06:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   const year = currentDate.getFullYear();
   const month = String(currentDate.getMonth() + 1).padStart(2, '0');
   const folderPath = `${year}/${month}/`;
-
+  console.log("folderpath is"$folderpath)
   fetch(folderPath)
     .then(response => {
       if (response.ok) {
