94ebbe6f2560e6144a49ffdd02ffbf63a890b21c
keymeat2025 <keymeat2025@gmail.com>
2025-04-02 17:56:02 +0530

    Update registration_firebase_config.js

--- registration_firebase_config.js
+++ registration_firebase_config.js
@@ -1,14 +1,17 @@
 // Firebase configuration
 // Store this in a separate file that's not exposed to public repositories
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
 
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlYM8lU1844ushiA7A3FtQPEtreQjJ30I",
+  authDomain: "letsgoto-a630e.firebaseapp.com",
+  projectId: "letsgoto-a630e",
+  storageBucket: "letsgoto-a630e.firebasestorage.app",
+  messagingSenderId: "194513214154",
+  appId: "1:194513214154:web:7825241b22095d7c43f233",
+  measurementId: "G-N8CN6LM82S"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
